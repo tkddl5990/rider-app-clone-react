9400fc19f7a7402e882bd05113795d4f816db990
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-04-03 12:54:42 +0900

    feat: lint 설정 및 path alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
   rules: {
     'linebreak-style': 0,
     'import/prefer-default-export': 0,
@@ -9,5 +15,33 @@ module.exports = {
     'import/no-unresolved': 0,
     'import/extensions': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
+  overrides: [
+    {
+      files: ['*.tsx', '**/*.ts', '**/*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // react-dom and react
+              ['^react$', '^react-dom', '^react-router'],
+              // external libraries
+              ['^[a-z]'],
+              // path alias
+              ['^@'],
+              // Parent imports.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Side effect imports
+              ['^\\u0000'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
