828ddf76568d20a4bf96984adaf70dc5efdcba7d
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-04-24 19:25:45 +0900

    feat: 로고 스타일 수정

--- src/Pages/Login/Login.css.ts
+++ src/Pages/Login/Login.css.ts
@@ -1,21 +1,22 @@
+import { flex } from '@Styles/Common.css';
 import { vars } from '@Styles/Theme.css';
 import { style } from '@vanilla-extract/css';
 
-export const container = style({
-  height: '100vh',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column',
-});
+export const container = style([
+  flex('center', 'center'),
+  {
+    flexDirection: 'column',
+    height: '100vh',
+  },
+]);
 
-export const logo = style({
-  width: '13rem',
-  height: '13rem',
-  backgroundColor: vars.color.gray[800],
-  color: vars.color.gray[100],
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  marginBottom: '8rem',
-});
+export const logo = style([
+  flex('center', 'center'),
+  {
+    width: '13rem',
+    height: '13rem',
+    backgroundColor: vars.color.gray[800],
+    color: vars.color.gray[100],
+    marginBottom: '8rem',
+  },
+]);
