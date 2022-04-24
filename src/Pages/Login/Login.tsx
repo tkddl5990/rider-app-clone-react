2cbce1ebf062b146402af14e1bd5ffb58a99ee82
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-04-24 22:03:16 +0900

    feat: 로그인 1차 구현

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,18 +1,45 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import * as style from './Login.css';
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const redirection = () => navigate('/', { replace: true });
+
+  useEffect(() => {
+    if (localStorage.getItem('utk')) redirection();
+  }, []);
+
   const onSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    localStorage.setItem('utk', 'hello world');
+    setEmail('');
+    setPassword('');
+    redirection();
   };
 
   return (
     <div className={style.container}>
       <h1 className={style.logo}>LOGO</h1>
       <form className={style.formWrapper} onSubmit={onSubmit}>
-        <input type='email' className={style.input} placeholder='email' />
-        <input type='password' className={style.input} placeholder='password' />
+        <input
+          type='text'
+          className={style.input}
+          placeholder='insert ID'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          className={style.input}
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type='submit' className={style.submitButton}>
           로그인
         </button>
